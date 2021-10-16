98e2db9c02d2194dbb3e61b984078a90012730f1
Tomasz Jader <tomasz.jader@gamil.com>
2021-10-17 01:09:48 +0200

    add 2 react components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import './App.css';
 import React, { useState } from "react";
 
+const countValidator =( count ) => count < 0 ? 0 : count;
+
+const Header = () => <h1 className="main__header">hydrapp</h1>;
+
+const Glass = (props) => {
+  const { count } = props;
+  return (<div className="main__glass">
+    <span className="main__value value__js">{count}</span>
+  </div>)
+}
+
 const App = (props) => {
   const [count, setCount] = useState(0);
 
   return (
     <main className="main">
-      <h1 className="main__header">hydrapp</h1>
-      <div className="main__glass">
-        <span className="main__value value__js">{ count}</span>
-      </div>
+      <Header></Header>
+      <Glass count={count} ></Glass>
       <button
         className="main__add add__button__js"
         onClick={() => {
-          
-          setCount(count+1);
+          setCount(count + 1);
         }}
-      >
-        dodaj szklankę
-      </button>
+      >dodaj szklankę</button>
       <button
         className="main__subtract subtract__button__js"
         onClick={() => {
-          setCount(count-1);
+          setCount(countValidator(count - 1));
         }}
       >
         usuń szklankę
