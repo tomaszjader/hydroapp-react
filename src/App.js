a3fc5ccd7f1df43040efe170d3a7e72033e00d74
Tomasz Jader <tomasz.jader@gamil.com>
2021-10-17 00:33:52 +0200

    couter witch useState hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
+import React, { useState } from "react";
 
-const App =()=> {
+const App = (props) => {
+  const [count, setCount] = useState(0);
+
   return (
-    <main class="main">
-      <h1 class="main__header">hydrapp</h1>
-      <div class="main__glass">
-        <span class="main__value value__js">0</span>
+    <main className="main">
+      <h1 className="main__header">hydrapp</h1>
+      <div className="main__glass">
+        <span className="main__value value__js">{ count}</span>
       </div>
-      <button class="main__add add__button__js">dodaj szklankę</button>
-      <button class="main__subtract subtract__button__js">usuń szklankę</button>
+      <button
+        className="main__add add__button__js"
+        onClick={() => {
+          
+          setCount(count+1);
+        }}
+      >
+        dodaj szklankę
+      </button>
+      <button
+        className="main__subtract subtract__button__js"
+        onClick={() => {
+          setCount(count-1);
+        }}
+      >
+        usuń szklankę
+      </button>
     </main>
   );
-}
+};
 
 export default App;
