52eed0e905a676a6436be0feb5c919edc9f56eee
Tomasz Jader <tomasz.jader@gamil.com>
2021-10-18 00:38:16 +0200

    add save data in localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const countValidator =( count ) => count < 0 ? 0 : count;
 
@@ -8,27 +8,41 @@ const Header = () => <h1 className="main__header">hydrapp</h1>;
 const Glass = (props) => {
   const { count } = props;
   return (<div className="main__glass">
-    <span className="main__value value__js">{count}</span>
+    <span className="main__value">{count}</span>
   </div>)
 }
 
 const App = (props) => {
   const [count, setCount] = useState(0);
+  const [dateKey, setDateKey] = useState('');
+ useEffect(() => {
+   const date = new Date();
+   setDateKey(date.toLocaleDateString());
 
+   let realValue = localStorage.getItem(dateKey)
+     ? localStorage.getItem(dateKey)
+     : localStorage.setItem(dateKey, 0);
+   realValue = parseInt(localStorage.getItem(dateKey));
+
+   setCount(realValue);
+ }, [dateKey]);
   return (
     <main className="main">
       <Header></Header>
       <Glass count={count} ></Glass>
       <button
-        className="main__add add__button__js"
+        className="main__add"
         onClick={() => {
           setCount(count + 1);
+          console.log(count);
+          localStorage.setItem(dateKey, count);
         }}
       >dodaj szklankę</button>
       <button
-        className="main__subtract subtract__button__js"
+        className="main__subtract"
         onClick={() => {
           setCount(countValidator(count - 1));
+          localStorage.setItem(dateKey, count);
         }}
       >
         usuń szklankę
